21c31de6f8d1dc3094d41f1b8e1056645a48fa94
Aleksandr Davydenko <ozziaqueen@gmail.com>
2025-03-25 18:28:04 +0100

    final

--- src/server.js
+++ src/server.js
@@ -37,6 +37,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -56,6 +57,7 @@ export const setupServer = () => {
 
     // Відповідь, якщо контакт знайдено
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
