8ee530201d8e3ca64c2b57e494f7b9c7f8f38b92
Aleksandr Davydenko <ozziaqueen@gmail.com>
2025-04-27 09:49:16 +0200

    final_r

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -13,13 +13,13 @@ const parseIsFavourite = (isFavourite) => {
 };
 
 export const parseFilterParams = (query) => {
-  const { type, isFavourite } = query;
+  const { contactType, isFavourite } = query;
 
-  const parsedType = parseType(type);
+  const parsedType = parseType(contactType);
   const parsedIsFavourite = parseIsFavourite(isFavourite);
 
   return {
-    type: parsedType,
+    contactType: parsedType,
     isFavourite: parsedIsFavourite,
   };
 };
